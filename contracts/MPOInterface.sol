f398f1c9142e28886cae8821c6fa251fb5b201f4
minciong <maxraphael_inciong@yahoo.com>
2018-06-18 18:22:56 -0400

    initialized getters and setters and structs within storage and interface

--- contracts/MPOInterface.sol
+++ contracts/MPOInterface.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.19
 
 interface MPOInterface(){
-
-
+	function addResponse(uint queryId, address party, string partyResponse);
+	function setThreshold(uint queryId, uint threshold);
+	function getResponses(uint queryId);
+	function getThreshold(uint queryId);
 }
--- contracts/MPOStorage.sol
+++ contracts/MPOStorage.sol
@@ -2,11 +2,35 @@ pragma solidity ^0.4.19;
 
 contract MPOStorage{
 
-	struct response{
+	struct Response{
 		address party;
 		string res;
 	}
-	mapping(response=>uint) queryToResponses;
-	uint[] queries;
-	
+	mapping(uint => Response[]) queryToResponses;
+	mapping(uint => uint) responseThreshold;
+//TODO maybe have a thresholdFull event?
+
+	//Set Methods/Mutators
+	function addResponse(
+		uint queryId, 
+		address party, 
+		string partyResponse){
+		require(queryToResponses[queryId].length <= responseThreshold[queryId]);
+
+		queryToResponses[queryId].push(new Response(party, partyResponse));
+	}
+	function setThreshold(
+		uint queryId, 
+		uint threshold){
+		responseThreshold[queryId] = threshold;
+	}
+
+	//Get Methods/Accessors
+	function getResponses(uint queryId) returns(Response[]){
+		return queryToResponses[queryId];
+	}
+	function getThreshold(uint queryId) returns(uint){
+		return responseThreshold[queryId];
+	}
+
 }
