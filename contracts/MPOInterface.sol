d34879aadd2796008661191c7e8724dec800764e
minciong <maxraphael_inciong@yahoo.com>
2018-06-19 16:00:38 -0400

    decided to move submitresponse to main contract. Made most accessors/mutators

--- contracts/MPOInterface.sol
+++ contracts/MPOInterface.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.4.19
 
 interface MPOInterface(){
-	function addResponse(uint256, address, string);
+	function submitResponse1(uint256, address, string);
 	function setThreshold(uint256, uint256);
+	function setResponders(address[])
 	function getResponses(uint256);
 	function getThreshold(uint256);
 }
--- contracts/MPOStorage.sol
+++ contracts/MPOStorage.sol
@@ -3,32 +3,34 @@ pragma solidity ^0.4.19;
 contract MPOStorage{
 
 	struct Response{
-		bool approved;
 		string responseString;
 	}
 	
 //TODO maybe have a thresholdFull event?
+	mapping(uint => string[]) queryResponses;
+	mapping(uint256 => uint256) responseThreshold
+	mapping(address => bool) approvedAddress;
+	uint256 threshold;
 	address[] responders;
 	//Set Methods/Mutators
-	function submitResponse1(
-		uint256 queryId, 
-		string response){
-		
-	}
-	function setThreshold(
-		uint256 queryId, 
-		uint256 threshold){
-		responseThreshold[queryId] = threshold;
+
+	function setThreshold(uint threshold){
+		this.threshold = threshold;
 	}
 	function setResponders(address[] parties){
-		
+		for(int i=0;i<parties.length;i++){
+			responders.push(parties[i]);
+			approvedAddress[parties[i]]=true;
+		}
+			
 	}
 	//Get Methods/Accessors
 	function getResponses(uint256 queryId) returns(Response[]){
-		return queryToResponses[queryId];
+		// return responseTally[queryId];
 	}
 	function getThreshold(uint256 queryId) returns(uint){
-		return responseThreshold[queryId];
+		return threshold;
 	}
+		}
 
 }
