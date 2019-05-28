658d5657cddec47b0992cfb86c02bd1afec7f87d
minciong <maxraphael_inciong@yahoo.com>
2019-05-28 13:54:20 -0400

    now able to deploy locally

--- migrations/3_contract_migration.js
+++ migrations/3_contract_migration.js
@@ -1,17 +1,77 @@
 //var Migrations = artifacts.require("./Migrations.sol");
 var MPOStorage = artifacts.require("./MPOStorage.sol");
 var MultiPartyOracle = artifacts.require("./MultiPartyOracle.sol");
+const ZapCoordinator = artifacts.require('./ZapCoordinator.sol');
+const Database = artifacts.require('./Database.sol');
 
+const Arbiter = artifacts.require("./Arbiter.sol");
+const Bondage = artifacts.require("./Bondage.sol");
+const CurrentCost = artifacts.require("./CurrentCost.sol");
+const Dispatch = artifacts.require("./Dispatch.sol");
+const Registry = artifacts.require("./Registry.sol");
+
+const Faucet = artifacts.require("./Faucet.sol");
+const Telegram = artifacts.require("./Telegram.sol");
+const ZapToken = artifacts.require("./ZapToken.sol");
 const deploy = async function(deployer, network) {
 
   //deployer.deploy(Migrations);
-  await deployer.deploy(MPOStorage);
-  const storageInstance = await MPOStorage.deployed();
-  await deployer.deploy(MultiPartyOracle, "0x0014f9acd4f4ad4ac65fec3dcee75736fd0a0230", MPOStorage.address, MPOStorage.address)
-  const MPOInstance = MultiPartyOracle.deployed();
-  await storageInstance.transferOwnership(MultiPartyOracle.address);
-  await MPOInstance.setup(["0xdbFE35a91EaA4c97456238ee60C48D0708272242",  "0x1096C36e95A3A1cff73d8d006C9dDEaAe5B85eCc",  "0x7e75C377c183A8f69eE57E2cCdAa3A016f5a5903"])
-  
+  if(network == "development"){
+    console.log("Deploying main contracts on: " + network);
+
+    // Deploy all the coordinator
+    console.log('Deploying the coordinator');
+    await deployer.deploy(ZapCoordinator);
+    const coordInstance = await ZapCoordinator.deployed();
+    const owner = await coordInstance.owner();
+
+    // Setup important contracts
+    console.log('Deploying and instantiating important contracts');
+    await deployer.deploy(Database);
+    const dbInstance = await Database.deployed();
+    await dbInstance.transferOwnership(ZapCoordinator.address);
+
+    await coordInstance.addImmutableContract('ZAP_TOKEN', ZapToken.address);
+    await coordInstance.addImmutableContract('DATABASE', Database.address);
+
+    // Deploy the rest of the contracts
+    console.log('Deploying the rest of the contracts');
+    await deployer.deploy(Registry, ZapCoordinator.address);
+    await deployer.deploy(CurrentCost, ZapCoordinator.address);
+    await deployer.deploy(Bondage, ZapCoordinator.address);
+    await deployer.deploy(Arbiter, ZapCoordinator.address);
+    await deployer.deploy(Dispatch, ZapCoordinator.address);
+
+    // Add the above contracts to the coordinator 
+    console.log('Adding contracst to the coordinator');
+    await coordInstance.updateContract('REGISTRY', Registry.address);
+    await coordInstance.updateContract('CURRENT_COST', CurrentCost.address);
+    await coordInstance.updateContract('BONDAGE', Bondage.address);
+    await coordInstance.updateContract('ARBITER', Arbiter.address);
+    await coordInstance.updateContract('DISPATCH', Dispatch.address);
+
+    console.log('Updating all the dependencies');
+    await coordInstance.updateAllDependencies({ from: owner });
+
+    // Deploy telegram example
+    console.log('Done migrating core contracts');
+    await deployer.deploy(MPOStorage);
+    console.log(typeof(MPOStorage.address));
+    const storageInstance = await MPOStorage.deployed();
+    await deployer.deploy(MultiPartyOracle, ZapCoordinator.address, MPOStorage.address, "0x00")
+    const MPOInstance = await MultiPartyOracle.deployed();
+    await storageInstance.transferOwnership(MultiPartyOracle.address);
+    await MPOInstance.setup(["0xdbFE35a91EaA4c97456238ee60C48D0708272242",  "0x1096C36e95A3A1cff73d8d006C9dDEaAe5B85eCc",  "0x7e75C377c183A8f69eE57E2cCdAa3A016f5a5903"])
+	}
+  else{
+    await deployer.deploy(MPOStorage);
+    console.log(typeof(MPOStorage.address));
+    const storageInstance = await MPOStorage.deployed();
+    await deployer.deploy(MultiPartyOracle, "0x0a07859b0063dfa1881f534791516ce7615bd0f7", MPOStorage.address, "0x00")
+    const MPOInstance = await MultiPartyOracle.deployed();
+    await storageInstance.transferOwnership(MultiPartyOracle.address);
+    await MPOInstance.setup(["0xdbFE35a91EaA4c97456238ee60C48D0708272242",  "0x1096C36e95A3A1cff73d8d006C9dDEaAe5B85eCc",  "0x7e75C377c183A8f69eE57E2cCdAa3A016f5a5903"])
+    }
 };
 module.exports = (deployer, network) => {
     deployer.then(async () => await deploy(deployer, network));
